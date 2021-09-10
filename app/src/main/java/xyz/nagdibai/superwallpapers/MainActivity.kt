b7003a9aec10b745de06f049a6ea98e242fbaa06
Sanved <sanved77@gmail.com>
2021-09-10 03:03:21 -0700

    Thembe thembe tale saache

--- app/src/main/java/xyz/nagdibai/superwallpapers/MainActivity.kt
+++ app/src/main/java/xyz/nagdibai/superwallpapers/MainActivity.kt
@@ -1,7 +1,14 @@
 package xyz.nagdibai.superwallpapers
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+import android.view.WindowManager
+import androidx.core.view.WindowCompat
 import xyz.nagdibai.superwallpapers.databinding.HomeMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +20,33 @@ class MainActivity : AppCompatActivity() {
         bnd = HomeMainBinding.inflate(layoutInflater)
         val view = bnd.root
         setContentView(view)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        // Enables regular immersive mode
+        if (Build.VERSION.SDK_INT in 21..29) {
+            window.statusBarColor = Color.TRANSPARENT
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.decorView.systemUiVisibility =
+                SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or SYSTEM_UI_FLAG_LAYOUT_STABLE
+
+        } else if (Build.VERSION.SDK_INT >= 30) {
+            window.statusBarColor = Color.TRANSPARENT
+            // Making status bar overlaps with the activity
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+        }
+    }
+
+    // Shows the system bars by removing all the flags
+    private fun showSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
     }
 }
